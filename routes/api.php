49ae1a6f7e19f3892c033332af57257b534f06f5
Evgeny <evgenygost@gmail.com>
2020-04-09 19:06:38 +0400

    User profile test (fetch posts for a profile)

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,6 @@ Route::middleware('auth:api')->group(function () {
     Route::apiResources([
         'posts' => 'PostController',
         'users' => 'UserController',
+        'users/{user}/posts' => 'UserPostController',
     ]);
 });
